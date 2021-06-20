71d07651dd603773184545bc890b48f828438b99
JonasEberlein98 <j.eberlein98@gmail.com>
2021-06-20 21:05:57 +0200

    -

--- C45_PROMISE.R
+++ C45_PROMISE.R
@@ -3,10 +3,18 @@ library(tidymodels)
 library(readr)
 library(RWeka)
 library(readxl)
+library(caTools)
 data <- read_excel("~/Desktop/data.xlsx")
-factor_data <- lapply(data, as.factor)
-fit <- J48(bugbinary~., data=factor_data)
+data <- 
+                         data %>% 
+                                                       mutate(bugbinary = factor(bugbinary))
+spl = sample.split(data$bugbinary, SplitRatio = 0.7)
+spl = sample.split(data$bugbinary, SplitRatio = 0.7)
+dataTrain = subset(data, spl==TRUE)
+dataTest = subset(data, spl==FALSE)
+trainJ48 <- J48(bugbinary~., dataTrain)
+dataTest.pred <- predict(trainJ48, newdata = dataTest)
+table(dataTest$bugbinary, dataTest.pred)
+precision(data, dataTest$bugbinary, dataTest.pred)
 
-predictions <- predict(fit, factor_data)
-table(predictions, factor_data$bugbinary)
-#change
+
