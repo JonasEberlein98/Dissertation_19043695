2011186fc6ba354d626408e5738ecd5ffbff41a5
JonasEberlein98 <j.eberlein98@gmail.com>
2021-06-21 19:18:29 +0200

    -

--- C45_PROMISE.R
+++ C45_PROMISE.R
@@ -9,12 +9,11 @@ data <-
                          data %>% 
                                                        mutate(bugbinary = factor(bugbinary))
 spl = sample.split(data$bugbinary, SplitRatio = 0.7)
-spl = sample.split(data$bugbinary, SplitRatio = 0.7)
 dataTrain = subset(data, spl==TRUE)
 dataTest = subset(data, spl==FALSE)
 trainJ48 <- J48(bugbinary~., dataTrain)
 dataTest.pred <- predict(trainJ48, newdata = dataTest)
 table(dataTest$bugbinary, dataTest.pred)
 precision(data, dataTest$bugbinary, dataTest.pred)
-
+f_meas(data, dataTest$bugbinary, dataTest.pred)
 
