ff544f61da4af7b3ea0888481282360b5385fd4f
JonasEberlein98 <j.eberlein98@gmail.com>
2021-07-04 18:45:32 +0200

    success

--- Silge_tidy.R
+++ Silge_tidy.R
@@ -20,11 +20,6 @@ data_boot <- bootstraps(data_train)
 data_boot
 
 #3 model specifications
-glm_spec <- logistic_reg() %>%
-  set_engine("glm")
-rf_spec <- rand_forest() %>%
-  set_mode("classification") %>%
-  set_engine("ranger")
 C50_spec <- decision_tree() %>%
   set_mode("classification") %>%
   set_engine("C5.0")
@@ -38,17 +33,9 @@ NN_spec <- mlp() %>%
 
 #4 workflow
 data_wf <- workflow() %>%
-  add_formula(bugbinary ~ .)
+  add_formula(bugbinary ~ .) %>%
 
 #5 add models
-glm_rs <- data_wf %>%
-   add_model(glm_spec) %>%
-   fit_resamples(resamples = data_boot, control = control_resamples(save_pred = TRUE))
-
-rf_rs <- data_wf %>%
-  add_model(rf_spec) %>%
-  fit_resamples(resamples = data_boot, control = control_resamples(save_pred = TRUE))
-
 C50_rs <- data_wf %>%
   add_model(C50_spec) %>%
   fit_resamples(resamples = data_boot, control = control_resamples(save_pred = TRUE))
@@ -62,41 +49,26 @@ NN_rs <- data_wf %>%
   fit_resamples(resamples = data_boot, control = control_resamples(save_pred = TRUE))
 
 #6 Evaluation
-collect_metrics(rf_rs)
-collect_metrics(glm_rs)
-collect_metrics(C50_rs)
-collect_metrics(NB_rs)
-collect_metrics(NN_rs)
+#collect_metrics(rf_rs)
+#collect_metrics(glm_rs)
+#collect_metrics(C50_rs)
+#collect_metrics(NB_rs)
+#collect_metrics(NN_rs)
 
 #6 final models with last_fit()
-#glm
-glm_final <- data_wf %>%
-  add_model(glm_spec) %>%
-  last_fit(data_split)
-collect_metrics(glm_final)
-collect_predictions(glm_final) %>%
-  conf_mat(bugbinary, .pred_class)
-
-#rf
-rf_final <- data_wf %>%
-            add_model(rf_spec) %>%
-            last_fit(data_split)
-collect_metrics(rf_final)
-collect_predictions(rf_final) %>%
-  conf_mat(bugbinary, .pred_class)
 
 #C50
 C50_final <- data_wf %>%
   add_model(C50_spec) %>%
-  last_fit(data_split)
+  last_fit(data_split) %>%
 collect_metrics(C50_final)
 collect_predictions(C50_final) %>%
   conf_mat(bugbinary, .pred_class)
-
+ 
 #NB
 NB_final <- data_wf %>%
   add_model(NB_spec) %>%
-  last_fit(data_split)
+  last_fit(data_split) 
 collect_metrics(NB_final)
 collect_predictions(NB_final) %>%
   conf_mat(bugbinary, .pred_class)
@@ -108,3 +80,21 @@ NN_final <- data_wf %>%
 collect_metrics(NN_final)
 collect_predictions(NN_final) %>%
   conf_mat(bugbinary, .pred_class)
+
+
+
+
+
+
+#metrics attempt
+c_metrics <- metric_set(accuracy, f_meas, mcc, roc_auc)
+model_rec <- recipe(bugbinary~., data = data_train)
+
+C50_tune <- tune_grid(
+  C50_spec,
+  model_rec,
+  resamples = data_boot,
+  metrics = c_metrics
+)
+C50_tune %>% 
+  collect_metrics()
